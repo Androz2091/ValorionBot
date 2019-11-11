d81e956e8311d9347d9cde33bf730e369097aa73
Androz2091 <androz2091@gmail.com>
2019-11-11 16:10:51 +0100

    Update titles

--- commands/help.js
+++ commands/help.js
@@ -18,20 +18,20 @@ class Help extends Command {
         let prefix = this.client.config.prefix;
 
         let helpEmbed = new Discord.RichEmbed()
-            .setAuthor("Liste des commandes")
-            .addField(this.client.config.emotes.tools+" Outils",
+            .addField("__**Liste des commandes :**__",
+                "Menu d'aide du Bot ! Apprenez toutes les commandes :"
+            )
+            .addField(this.client.config.emotes.tools+" **Outils**",
                 "`"+prefix+"liens` : Affiche les principaux liens de Valorion !\n"+
                 "`"+prefix+"invite` : Inviter le bot sur votre serveur !\n"+
                 "`"+prefix+"conseil` : Vous donne quelques conseils pour Valorion !\n"
             )
-            .addField(this.client.config.emotes.stats+" Stats",
+            .addField(this.client.config.emotes.stats+" **Stats**",
                 "`"+prefix+"setup` : Recréer les salons de stats !\n"+
                 "`"+prefix+"status` : Affiche les informations de Valorion !"
             )
-            .addField(this.client.config.emotes.infos+" Informations",
-                "[Invitation](https://discordapp.com/oauth2/authorize?client_id="+this.client.user.id+"&permissions=2146958847&scope=bot) | "+
-                "[Notre Discord]("+this.client.config.supportInvite+") | "+
-                "[Voter](https://discordbots.org/bot/"+this.client.user.id+")"
+            .addField(this.client.config.emotes.infos+" **Informations**",
+                "[Invitation](https://discordapp.com/oauth2/authorize?client_id="+this.client.user.id+"&permissions=2146958847&scope=bot)"
             )
             .setFooter("Valorion'Bot, les stats de Valorion en temps réel.")
             .setThumbnail("https://i.ytimg.com/vi/ZdFpaB4Wbn0/maxresdefault.jpg")
