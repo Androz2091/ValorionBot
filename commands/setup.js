dc43c10fa6a60d48277193884a3f09e76672cfa4
Androz2091 <androz2091@gmail.com>
2019-11-11 16:12:08 +0100

    feat(commands): update markdown

--- commands/setup.js
+++ commands/setup.js
@@ -14,11 +14,11 @@ class Setup extends Command {
     }
 
     async run (message, args, data) {
-        let m = await message.channel.send(this.client.emotes.loading+" | Création des salons en cours...");
+        let m = await message.channel.send(this.client.emotes.loading+" **| Création des salons en cours...**");
         await this.client.functions.clearGuild(this.client, message.guild.id);
         await this.client.functions.createStatsChannels(this.client, message.guild);
         await this.client.functions.updateStats(this.client, message.guild.id);
-        m.edit(this.client.emotes.success+" | Les salons ont été créés et sont opérationnels !");
+        m.edit(this.client.emotes.success+" **| Les salons ont été créés et sont opérationnels !**");
     }
 
 }
