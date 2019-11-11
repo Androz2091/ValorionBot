28716661e8be403cb9181f05e33f3c6ce9f70a6f
Androz2091 <androz2091@gmail.com>
2019-11-11 16:11:11 +0100

    feat(liens): add more links

--- commands/liens.js
+++ commands/liens.js
@@ -16,32 +16,28 @@ class Liens extends Command {
     async run (message, args, data) {
 
         let linksEmbed = new Discord.RichEmbed()
-            .addField("Principaux liens",
-                "Retrouvez les principaux liens utiles de Valorion !"
+            .addField("Principaux liens de Valorion",
+                "Retrouvez les liens utiles de Valorion !"
             )
-            .addField("Site",
+            .addField("Site internet",
                 "[Accueil](https://valorion-mc.fr)\n"+
                 "[Boutique](https://www.valorion-staff.fr/shop)\n",
                 true
             )
-            .addField("Launchers",
+            .addField("Launchers de Valorion :",
                 "[Launcher Premium](https://valorion-staff.fr/p/launcher_premium)\n",
+                "[Launcher Crack](https://valorion-staff.fr/p/launcher_crack)\n",
                 true
             )
-            .addField("Waren - YouTube",
-                "[V2](https://www.youtube.com/playlist?list=PLoTOukWpJpv0M02m_c_ch1U75N6w1IXQK) | "+
-                "[V1](https://www.youtube.com/playlist?list=PLoTOukWpJpv3QpYZIi7g6BSL2GhX32e9H)\n"+
+            .addField("YouTube | Discord",
+                "[V2](https://www.youtube.com/playlist?list=PLoTOukWpJpv0M02m_c_ch1U75N6w1IXQK) | ",+
+                "[V1](https://www.youtube.com/playlist?list=PLoTOukWpJpv3QpYZIi7g6BSL2GhX32e9H)\n",
+                "[Chaîne Communautaire](https://www.youtube.com/channel/UCfCAMuhANlTSrkxjxB84HLA)\n",                            
                 "[Discord de Waren](https://discord.gg/warentv)\n",
                 true
             )
-            .addField("Support de Valorion",
+            .addField("**Discord de Valorion**",
                 "[Discord](https://discordapp.com/invite/pmYMswG)\n",
-                true
-            )
-            .addField("Valorion'Bot",
-                "[Invitation](https://discordapp.com/oauth2/authorize?client_id="+this.client.user.id+"&permissions=2146958847&scope=bot) | "+
-                "[Voter](https://discordbots.org/bot/"+this.client.user.id+"/vote) | "+
-                "[Support]("+this.client.config.supportInvite+")",
                 true
             )
             .setFooter("Une idée pour améliorer le bot ? Rejoignez notre Discord !")
